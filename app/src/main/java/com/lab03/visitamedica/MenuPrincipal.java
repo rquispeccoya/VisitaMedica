076838cbc1b3c419a4f3daec38f588a651e8f47e
Royer <rquispeccoya@gmail.com>
2021-10-01 17:32:50 -0500

    Subiendo a git

---
+++ app/src/main/java/com/lab03/visitamedica/MenuPrincipal.java
@@ -0,0 +1,14 @@
+package com.lab03.visitamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
