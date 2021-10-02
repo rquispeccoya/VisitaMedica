51bf40d53671ec21d4d781f62d70adc0620db4dd
Royer <rquispeccoya@gmail.com>
2021-10-01 21:30:39 -0500

    agregacion del widgets formulario , paso de parametros pero aun por arreglar

--- app/src/main/java/com/lab03/visitamedica/FormularioPacienteActivity.java
+++ app/src/main/java/com/lab03/visitamedica/FormularioPacienteActivity.java
@@ -1,13 +1,48 @@
 package com.lab03.visitamedica;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class FormularioPacienteActivity extends AppCompatActivity {
-
+    Button btnRegistrar ;
+    EditText editTextDni;
+    EditText editTextNombres;
+    EditText editTextApellidos;
+    EditText editTextDireccion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_paciente);
+
+        editTextDni=findViewById(R.id.editTextNumber);
+        editTextNombres=(EditText) findViewById(R.id.editTextNombresPaciente);
+        editTextApellidos=findViewById(R.id.editTextApellidosPaciente);
+        editTextDireccion=findViewById(R.id.editTextDireccionPaciente);
+        btnRegistrar=findViewById(R.id.buttonRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //String dni = editTextDni.getText().toString();
+                String nombres=editTextNombres.getText().toString();
+                String apellidos=editTextApellidos.getText().toString();
+                String direccion=editTextDireccion.getText().toString();
+
+                Intent intent = new Intent();
+                //intent.putExtra("dni",dni);
+                intent.putExtra("nombres",nombres);
+                //intent.putExtra("apellidos",apellidos);
+                //intent.putExtra("direccion",direccion);
+                setResult(0,intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/lab03/visitamedica/MenuPrincipalActivity.java
+++ app/src/main/java/com/lab03/visitamedica/MenuPrincipalActivity.java
@@ -1,14 +1,54 @@
 package com.lab03.visitamedica;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MenuPrincipalActivity extends AppCompatActivity {
+public class MenuPrincipalActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnRegistrarPaciente ;
+    Button btnVisitaPaciente;
+    TextView txtInformacionPaciente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegistrarPaciente=findViewById(R.id.buttonRegistroPaciente);
+        btnVisitaPaciente=findViewById(R.id.buttonVisitaPaciente);
+
+        btnRegistrarPaciente.setOnClickListener(this);
+        //btnVisitaPaciente.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        if (view.getId()==R.id.buttonRegistroPaciente){
+            intent = new Intent(MenuPrincipalActivity.this, FormularioPacienteActivity.class);
+            startActivityForResult(intent,0);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //txtInformacionPaciente.setText(data.getStringExtra("dni"));
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) { // Activity.RESULT_OK
+
+                //txtInformacionPaciente.setText(data.getStringExtra("nombres"));
+                txtInformacionPaciente.setText("HOLAAA");
+            }
+        }
     }
 }
