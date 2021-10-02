8091e5d74350ba42b4049c60d6c51e78e4b9e771
Royer <rquispeccoya@gmail.com>
2021-10-01 19:31:00 -0500

    Creacion de los activities:FormularioPaciente y VisitaPaciente

---
+++ app/src/main/java/com/lab03/visitamedica/FormularioPacienteActivity.java
@@ -0,0 +1,13 @@
+package com.lab03.visitamedica;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioPacienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_paciente);
+    }
+}
